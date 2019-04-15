719c2357d8d2db2f728fa6d83a366f81f8d5be43
William Rivera <william.pr07@hotmail.com>
2019-04-15 17:24:56 -0400

    add YOURAPIKEYHERE on the url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-const express = require('express')
-const app = express()
-const request = require('request')
+const express = require('express');
+const request = require('request');
+const app = express();
 var http = require('http').Server(app);
 
 // Public folder for assets
@@ -11,16 +11,16 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // Main page route
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => res.render('home'));
 
 // about page route
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => res.render('about'));
 
 // results page route
 app.get('/results', function(req, res) {
     var query = req.query.search;
     var year = req.query.year;
-    var url = 'http://www.omdbapi.com/?apikey=1e829d0e&s=' + query + '&y=' + year;
+    var url = 'http://www.omdbapi.com/?apikey=YOURAPIKEYHERE&s=' + query + '&y=' + year;
 
     request(url, function(error, response, body) {
         var data = JSON.parse(body)
@@ -30,7 +30,7 @@ app.get('/results', function(req, res) {
 });
 
 // Display message if page or route don't exist
-app.get('*', (req, res) => res.send('Page NOT found!'))
+app.get('*', (req, res) => res.send('Page NOT found!'));
 
 // Start Server
 http.listen(process.env.PORT || 3000, function(){
